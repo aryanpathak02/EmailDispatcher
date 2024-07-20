9c3ab2bfb7e54f1d979fa06583093c4a61005d0c
aryanpathak02 <aryanpathak02@gmail.com>
2024-07-20 16:15:06 +0530

    Added cors of specific domain

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://aryanpathak.netlify.app/' 
+}));
 
 // Nodemailer transporter setup
 const transporter = nodemailer.createTransport({
@@ -25,7 +28,7 @@ app.post('/send-email', (req, res) => {
   const { name, comment, email } = req.body;
 
   if (Object.keys(req.body).length === 0) {
-    return res.status(400).send({ message: 'Request body is empty' });
+    return res.status(400).send({ message: 'Request body is empty'});
   }
 
   const mailOptions = {
